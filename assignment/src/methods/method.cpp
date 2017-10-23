fc6ea89989d7e48636b4f74a5cbbc91eb767516b
António Pedro Fraga <antoniopedrofraga@gmail.com>
2017-10-24 00:09:34 +0100

    saving state before replacing Wall to Problem

--- assignment/src/methods/method.cpp
+++ assignment/src/methods/method.cpp
@@ -1,15 +1,30 @@
 #include "method.h"
 
+// CONSTRUCTORS
+/*=
+ *Default constructor
+ */
 Method::Method() {}
 
+/*
+ * Alternate constructor - creates a method with a given problem
+ */
 Method::Method(Wall wall) {
 	this->wall = wall;
 }
 
+// PUBLIC ACCESSOR METHODS
+
+/*
+ * public accessor method - get the method name
+ */
 std::string Method::get_name() {
 	return name;
 }
 
+/*
+ * public accessor method - get the solution grid
+ */
 Matrix Method::get_grid() {
 	return (*wall.get_grid());
 }
--- assignment/src/methods/method.h
+++ assignment/src/methods/method.h
@@ -21,19 +21,38 @@ public:
 
 	/**
 	* Default constructor.  Intialize a Method object
-	* @see Matrix(int Nrows, int Ncols)
-	* @see Matrix(const Matrix& m)
+	* @see Method(Wall wall)
 	*/
 	Method();
 
 	/**
 	* Alternate constructor. Initializes a Method with a given parabolic problem.
-	* @see Matrix(int Nrows, int Ncols)
-	* @see Matrix(const Matrix& m)
+	* @see Method()
 	*/
 	Method(Wall wall);
+
+	// PUBLIC ACCESSOR METHODS
+
+	/**
+	* Normal public get method.
+	* get the method name
+	* @return string. Method name.
+	*/
 	std::string get_name();
+
+	/**
+	* Normal public get method.
+	* get the solution grid
+	* @return Matrix. Computed solution grid.
+	*/
 	Matrix get_grid();
+
+	// PUBLIC MUTATOR METHODS
+
+	/**
+	* A pure virtual member.
+	* compute the solution following the rules of a given method.
+	*/
 	virtual void compute_solution() = 0;
 };
 
